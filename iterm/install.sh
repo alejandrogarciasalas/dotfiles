4d75ea5805689f6076ecbf09f51865080fc022b8
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2019-01-05 20:12:37 -0500

    add iterm profile

---
+++ iterm/install.sh
@@ -0,0 +1,7 @@
+# blog post on how importing/exporting iterm profiles work:
+# http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
