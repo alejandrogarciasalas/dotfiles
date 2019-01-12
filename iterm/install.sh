72f1cef0bfaa3f852d2d7b0258a91246f7300c0c
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2019-01-11 20:29:33 -0500

    update

--- iterm/install.sh
+++ iterm/install.sh
@@ -5,3 +5,7 @@
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "/Users/alejandro/dotfiles/iterm"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+# Post install manual configuration for prettier iterm
+# https://codematters.blog/custom-iterm2-titlebar-background-colors-a088c6f2ec60
+# https://www.felixjung.io/posts/pretty-iterm2-with-a-modern-titlebar/
