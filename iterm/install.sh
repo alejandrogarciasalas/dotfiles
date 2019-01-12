3e4eaf09a586b1ec948c8d4850f5554bb3e470b0
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2019-01-11 20:22:35 -0500

    update iterm script

--- iterm/install.sh
+++ iterm/install.sh
@@ -2,6 +2,6 @@
 # http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
 
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "/Users/alejandro/dotfiles/iterm"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
