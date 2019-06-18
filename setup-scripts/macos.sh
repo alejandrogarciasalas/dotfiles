80f7139391cc1591c76bad0a86d1627f7e1aff1a
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2019-06-17 20:55:34 -0700

    update

--- setup-scripts/macos.sh
+++ setup-scripts/macos.sh
@@ -1,3 +1,4 @@
+# FIX: brew script won't find brewfile because of relative paths stuff
 sh ../brew/install.sh # Installing Homebrew and Brewfile
 sh ../macos/install.sh # setting up MacOS defaults
 sh ../iterm/install.sh # import iTerm profile
